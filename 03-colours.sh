db6f389198f9276ea5afdce475d3c325c046510f
suresh <suresh31346@gmail.com>
2023-08-18 10:33:17 +0530

    Adding 2 numbers

--- 03-colours.sh
+++ 03-colours.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "Testing colours format"
-$1 = echo "Enter first number: "
 echo " Your first number is " $1
-$2= echo "Enter your second number: "
 echo "Your second number is " $2
 Total = $1 + $2
 echo " Total is : " $Total
